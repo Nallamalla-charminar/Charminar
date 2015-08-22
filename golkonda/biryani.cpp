f3ea1eb749bca5d072af312d18f8ff2281acff8f
Nallamalla <nallamalla.ravisekhar@gmail.com>
2015-08-22 12:12:01 +0530

    Issue#0002 Description of cusine
    	 modified:   biryani.cpp
    	 modified:   kabab.net

---
+++ golkonda/biryani.cpp
@@ -0,0 +1,6 @@
+
+
+Excavations held in Akrotiri on the Greek island of Santorini by 
+professor Christos G. Doumas unearthed firedogs 
+(stone sets of barbecue for skewers; Greek: κρατευταί - krateutai[9]) 
+used before the 17th century BCE.
