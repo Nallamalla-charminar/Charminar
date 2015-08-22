dc31c15b006f34a00fa59d77ce9f454ae6657a64
Nallamalla <nallamalla.ravisekhar@gmail.com>
2015-08-22 14:38:28 +0530

    Issue#0006    modified:   iranichai.java
    	iranichai.java

---
+++ golkonda/iranichai.java
@@ -0,0 +1,5 @@
+
+
+Chuanr (Chinese: 串; pinyin: chuàn), often referred to as "chua'r" 
+in Pekingese and throughout the North, or kawap (كاۋاپ) in Uyghur, is a variation of kebab originating 
+from the Uyghurs in the Western province of Xinjiang and a popular dish in Chinese Islamic cuisine. 
